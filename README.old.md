4bab595dfa0545ea4c8c8c120f43774f908a3deb
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-11-14 18:42:08 +0200

    d

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# landing_-betlace
--- src/components/main/MainStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-const MainStyled = styled.main`
-  min-height: calc(100vh - 470px);
-  @media screen and (min-width: 1280px) {
-    min-height: calc(100vh - 615px);
-  }
-`;
-
-export default MainStyled;
